a13d3968ad700da6edd98ef30fbbdac23916d386
Anda <anda.nenu@888holdings.com>
2024-06-03 14:15:30 +0300

    feat: load async image

--- app/src/app/catalog/CatalogImage.tsx
+++ app/src/app/catalog/CatalogImage.tsx
@@ -3,9 +3,19 @@
 import { Photo } from '@/types';
 import Image from 'next/image';
 
+const ImageAsync = async ({ alt, src, ...props }: {
+  src: string;
+  height: number;
+  width: number;
+  alt: string;
+  className: string;
+}) => {
+  return <Image src={src} alt={alt} {...props} />;
+};
+
 export default async function CatalogImage({ image: { url, title } }: { image: Photo }) {
   return (
-    <Image
+    <ImageAsync
       src={url}
       width={600}
       height={600}
